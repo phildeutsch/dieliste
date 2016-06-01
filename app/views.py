4fe414156650e73724ec1afabb485ad6dcb6b23e
Philipp Deutsch <philipp.g.deutsch@gmail.com>
2016-06-01 13:08:05 +0100

    Added openid providers

--- app/views.py
+++ app/views.py
@@ -7,6 +7,11 @@ from .forms import LoginForm
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
     return render_template('login.html',
                            title='Sign In',
-                           form=form)
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
