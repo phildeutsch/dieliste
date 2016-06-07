4ac9e455f5563d10b7d629be77f4379806fe3430
Philipp Deutsch <philipp.g.deutsch@gmail.com>
2016-06-07 14:06:21 +0100

    Add all forms for testing

--- app/forms.py
+++ app/forms.py
@@ -2,8 +2,14 @@ from flask_wtf import Form
 from wtforms import TextField, IntegerField, DecimalField
 from wtforms.validators import DataRequired
 
+class userForm(Form):
+    user_name = TextField('user_name', validators=[DataRequired()])
+
+class tripForm(Form):
+    destination = TextField('destination', validators=[DataRequired()])
+
 class expenseForm(Form):
     user_name = TextField('user_name', validators=[DataRequired()])
-    trip_id = IntegerField('trip_id', validators=[DataRequired()])
+    trip_id = TextField('trip_id', validators=[DataRequired()])
     description = TextField('description', validators=[DataRequired()])
-    value = DecimalField('value', validators=[DataRequired()])
+    value = TextField('value', validators=[DataRequired()])
--- app/views.py
+++ app/views.py
@@ -1,15 +1,21 @@
 from flask import render_template, flash, redirect
 from app import app
-from .forms import expenseForm
+from .forms import userForm, tripForm, expenseForm
 from .models import User, Trip, Expense
 
 # index view function suppressed for brevity
 
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    form = expenseForm()
+    user_form = userForm()
+    trip_form = tripForm()
+    expense_form = expenseForm()
 
     return render_template("index.html",
                            title='Home',
+                           users=User.query.all(),
+                           trips=Trip.query.all(),
                            expenses=Expense.query.all(),
-                           form=form)
+                           user_form=user_form,
+                           trip_form=trip_form,
+                           expense_form=expense_form)
